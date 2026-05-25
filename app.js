35189f728c7b5785e7b179c6936ee23ca8f99f04
Antavion <manishsamal832@gmail.com>
2026-05-25 21:41:19 +0530

    Configure ICE servers for Peer connection
    Added ICE server configuration for Peer connection.

--- app.js
+++ app.js
@@ -43,7 +43,30 @@ function joinRoom() {
 }
 
 function initPeer(id) {
-  peer = new Peer(id, { debug: 0 });
+  peer = new Peer(id, {
+    debug: 0,
+    config: {
+      iceServers: [
+        { urls: "stun:stun.l.google.com:19302" },
+        { urls: "stun:stun1.l.google.com:19302" },
+        {
+          urls: "turn:openrelay.metered.ca:80",
+          username: "openrelayproject",
+          credential: "openrelayproject"
+        },
+        {
+          urls: "turn:openrelay.metered.ca:443",
+          username: "openrelayproject",
+          credential: "openrelayproject"
+        },
+        {
+          urls: "turn:openrelay.metered.ca:443?transport=tcp",
+          username: "openrelayproject",
+          credential: "openrelayproject"
+        }
+      ]
+    }
+  });
 
   peer.on("open", () => {
     showWatchScreen();
